e77af938a517fecd63a5943d5450214e993db5fa
Apoorva Lal <lal.apoorva@gmail.com>
2023-10-24 15:44:36 -0700

    added donnelly's magic dag trick

--- utils.py
+++ utils.py
@@ -1,9 +1,14 @@
 import numpy as np
 import pandas as pd
+import graphviz as gr
 
 def simulate(**kwargs):
   values = {}
+  g = gr.Digraph()
   for k,v in kwargs.items():
+    parents = v.__code__.co_varnames
     inputs = {arg: values[arg] for arg in v.__code__.co_varnames}
     values[k] = v(**inputs)
-  return pd.DataFrame(values)
+    for p in parents:
+      g.edge(p, k)
+  return pd.DataFrame(values), g
